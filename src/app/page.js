ceec2649dd889af42c5c679c5fdeef39eaf41bab
Nima77Moosavi <94492258+Nima77Moosavi@users.noreply.github.com>
2025-09-26 18:30:05 +0330

    meta title and description for home page

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,13 @@ import SpecialProducts from "@/components/SpecialProducts/SpecialProducts";
 import FeaturesLine from "@/components/FeaturesLine/FeaturesLine";
 import Footer from "@/components/Footer/Footer";
 
+export const metadata = {
+  title: "فروشگاه کیمیا ترنج | صنایع دستی اصفهان ",
+  description:
+    "خرید صنایع دستی اصیل اصفهان و ایران؛ خاتم‌کاری، قلم‌زنی، مینیاتور، سماور برنجی، پک هدیه سازمانی و محصولات دست‌ساز با کیفیت.",
+};
+
+
 export default function Home() {
   return (
     <div className={styles.page}>
