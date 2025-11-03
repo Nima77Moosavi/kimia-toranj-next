f8337d91373c4152ee1427c0654b7f6550cfe6af
Nima77Moosavi <94492258+Nima77Moosavi@users.noreply.github.com>
2025-11-03 09:40:33 +0330

    shop error

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import ShopClient from "./ShopClient";
 import FooterMenu from "@/components/FooterMenu/FooterMenu";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "فروشگاه صنایع دستی | کیمیا ترنج",
@@ -13,7 +14,9 @@ export default function ShopPage() {
   return (
     <>
       <Header />
-      <ShopClient />
+      <Suspense fallback={<div>Loading shop...</div>}>
+        <ShopClient />
+      </Suspense>
       <Footer />
       <FooterMenu />
     </>
