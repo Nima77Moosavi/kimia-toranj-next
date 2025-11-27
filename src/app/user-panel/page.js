5880182b50d4f87a1e69455aa13317f6e20241b0
Nima77Moosavi <94492258+Nima77Moosavi@users.noreply.github.com>
2025-11-27 11:04:47 +0330

    meta title

--- src/app/user-panel/page.js
+++ src/app/user-panel/page.js
@@ -1,6 +1,10 @@
 // app/user-panel/page.jsx
 import { redirect } from "next/navigation";
 
+export const metadata = {
+  title: "پنل کاربری، کیمیاترنج",
+};
+
 export default function UserPanelIndex() {
   redirect("/user-panel/wishlist");
 }
